2193036308533a608fd4a9f65ae34be92f512c32
bhupesh1108 <bhupesh.verma00700@gmail.com>
2024-03-19 23:55:13 +0530

    new

---
+++ mynotepad/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import App1 from './NoteProject/App1'
+function App() {
+  return (
+   <App1></App1>
+  );
+}
+
+export default App;
---
+++ mynotepad/src/NoteProject/notes.js
@@ -0,0 +1,13 @@
+const notes = [
+   {
+      key:1,
+      title:"Day1",
+      content:"3 DSA Questions",
+    }
+    ,{
+      key:2,
+      title:"Day2",
+      content:"Project",
+    }
+  ];
+  export default notes;
